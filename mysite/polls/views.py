f6ddc15dcb43d0ee1478f9bad191f5a264b199f7
blopez024 <bmlopez024@gmail.com>
2024-08-26 14:55:18 -0700

    Before changing detail function with get_object_or_404()

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,23 @@
+from django.http import HttpResponse, Http404
+
+# from django.template import loader
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "poll/index.html", context)
 
 
 def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return HttpResponse("You're looking at question %s." % question_id)
 
 
