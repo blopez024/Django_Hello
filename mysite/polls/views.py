db9e44a1bac8309c415484a8644bad8ee0194b41
blopez024 <bmlopez024@gmail.com>
2024-08-26 17:01:36 -0700

    Implement real vote view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,11 @@
-from django.http import HttpResponse, Http404
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from .models import Choice, Question
 
 # from django.template import loader
-from django.shortcuts import render, get_object_or_404
-from .models import Question
 
 # Create your views here.
 
@@ -24,4 +27,21 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice",
+            },
+        )
+    else:
+        selected_choice = F("votes") + 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse("polls:result", args=(question.id,)))
